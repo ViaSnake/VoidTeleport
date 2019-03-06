889b8f218a27d2f0e846d47adced62351e9272f9
snake <admin@viasnake.com>
2019-03-06 15:19:41 +0900

    Initial commit

---
+++ src/main/java/biz/mcplay/voidteleport/VoidTeleport.java
@@ -0,0 +1,31 @@
+package biz.mcplay.voidteleport;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoidTeleport extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("VoidTeleport is enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("VoidTeleport is disabled.");
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        Player player = e.getPlayer();
+        Location loc = player.getLocation();
+        if (loc.getY() <= 0.0) {
+            e.getPlayer().teleport(getServer().getWorlds().get(0).getSpawnLocation());
+        }
+    }
+}
+
